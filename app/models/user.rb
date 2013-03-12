f3cbb0c50984850b53196b187401b9a1109f95b9
Genevieve Gustafson <workgenevieve@gmail.com>
2013-03-11 22:04:03 -0700

    added validation for uniqueness and presence of email and username:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password_digest, :profile_bg, :profile_fg, :profile_image, :username
+  validates_presence_of :email, :username
+  validates_uniqueness_of :email, :username
 end
