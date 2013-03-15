f39f7f75dc33a2c5230d334ab60cc7f0ab9221bb
Genevieve Gustafson <workgenevieve@gmail.com>
2013-03-14 22:04:51 -0700

    added validation of username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password_digest, :profile_bg, :profile_fg, :profile_image, :username
   validates_presence_of :email, :username
   validates_uniqueness_of :email, :username
+  validates :password_digest, :presence => true, :on => :create
+  validates :username, :format => {:with => /^[a-z]\w*$/i, :message => "First Character Must be a Letter"}
 end
