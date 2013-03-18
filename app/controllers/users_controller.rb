e9a61edca1d974f566f6553f9a576c480291d325
Genevieve Gustafson <workgenevieve@gmail.com>
2013-03-18 15:16:04 -0700

    added shouts to user show.html.erb. paginated shouts in users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
-
+    @shouts = @user.shouts.order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
