346a5da5e176cbebb4c36b5663bb63951510ecc9
Genevieve Gustafson <workgenevieve@gmail.com>
2013-03-19 19:20:28 -0700

    added helpers for login process(current_user, logged_in?, etc). Change create on user to create a session,

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  private
+  
+  def current_user
+     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   end
+   
+   def logged_in?
+    current_user ? true : false
+   end
+    
+   def require_user
+     redirect_to sessions_new unless logged_in?     
+   end   
+   
+   def require_no_user
+     redirect_to root_path if logged_in?
+   end
+    
+   helper_method :current_user, :logged_in?, :require_user, :require_no_user
 end
